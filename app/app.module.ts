bb1126c14745b1e6863a304ed684cf0865b1eeb2
Andrei <a.zubrytski@andersenlab.com>
2017-05-11 20:50:28 +0300

    Add new version of project

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,37 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import{NgModule} from'@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { EventsAppComponent } from './events-app.component'
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './shared/data.service';
+
+import {AppComponent} from './app.component';
+import {TodoFormComponent} from './todo-form/todo-form.component';
+import {TodoListComponent} from './todo-list/todo-list.component';
+import {TodoItemComponent} from './todo-item/todo-item.component';
+import {TodoService} from './shared/todo.service';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [EventsAppComponent],
-  bootstrap: [EventsAppComponent]
+    imports:[
+        BrowserModule, 
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
+        
+    ],
+    declarations:[
+        AppComponent,
+        TodoFormComponent,
+        TodoListComponent,
+        TodoItemComponent
+        
+    ],
+    providers:[TodoService],
+    bootstrap:[AppComponent]
 })
-export class AppModule {}
+
+
+export class AppModule{
+
+}
